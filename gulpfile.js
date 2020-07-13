8116b3f0a3c84e7b9d0ab5f813c5b6ad39d3d875
victor mann <victorcmann@gmail.com>
2020-07-13 19:00:28 -0300

    tasks clean,js,css,images,sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,57 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const rename = require('gulp-rename');
+const uglifyjs = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+const image = require('gulp-image');
+const sass = require('gulp-sass');
 
-function clean(cb) {
-    cb();
-}
-function ren(cb) {
+sass.compiler = require('node-sass');
+
+function _clean(cb) {
     return gulp
-    .src('./src/*.js')
+    .src('./dist/**/!(*.html)')
+    .pipe(clean({force: true}));
+}
+function js(cb) {
+    return gulp
+    .src('./src/**/*.js')
+    .pipe(uglifyjs())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('./dist'));
 }
-
 function css(cb) {
-    cb();
+    return gulp
+    .src('./src/**/*.css')
+    .pipe(uglifycss())
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest('./dist'));
 }
 function images(cb) {
-    cb();
+    return gulp
+    .src('./src/**/*(*.png|*.jpg|*.jpeg|*.gif)')
+    .pipe(image())
+    .pipe(gulp.dest('./dist'));
+}
+function _sass(cb) {
+    return gulp
+    .src('./src/sass/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+}
+function _sass_watch() {
+    gulp.watch('./src/sass/*.scss', _sass);
 }
 
-function js(cb) {
-    cb();
-}
 
+exports.clean = _clean;
 exports.js = js;
-exports.default = ren;
-exports.build = gulp.parallel(css, js, images);
-exports.group = gulp.series(
-    clean,
-    gulp.parallel(
-        css,
-        js,
-        images
-    ),
-    ren
+exports.css = css;
+exports.images = images;
+exports.sass = _sass;
+exports['sass:watch'] = _sass_watch;
+exports.default = gulp.series(
+    clean, 
+    gulp.parallel(js, css, images)
 );
+exports.build = gulp.parallel(css, js, images);
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+var name = 'Richard Parker';
+
+/**
+ *  Realiza a soma de dois números
+ * @param {Number} x 
+ * @param {Number} y 
+ * @return {Number}
+ */
+function soma(x, y) {
+    return x + y;
+}
